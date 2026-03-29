8480f0800816678ddeb55f8a239dc657f3cb0f08
Soheil Nazari <kontakt@soheilnazari.de>
2026-03-29 21:47:15 +1100

    update octokit client

--- src/octokit.ts
+++ src/octokit.ts
@@ -1,14 +1,13 @@
 import { Octokit } from '@octokit/rest'
 import { setGlobalDispatcher, Agent } from 'undici'
 
-setGlobalDispatcher(
-  new Agent({
-    keepAliveTimeout: 1,
-    keepAliveMaxTimeout: 1,
-  }),
-)
-
 export function createOctokit(): Octokit {
+  setGlobalDispatcher(
+    new Agent({
+      keepAliveTimeout: 1,
+      keepAliveMaxTimeout: 1,
+    }),
+  )
   return new Octokit({
     auth: process.env.GH_TOKEN,
   })
